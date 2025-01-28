aac61002f44ec6884d74aed3f483126bbbe56821
v a r r <91899516+VarrelKun@users.noreply.github.com>
2025-01-29 01:32:10 +0700

    Update KomikList.jsx

--- src/components/KomikList.jsx
+++ src/components/KomikList.jsx
@@ -4,13 +4,14 @@ import Ongoing from "@/components/ListCard/Ongoing"
 import GenreList from "@/components/ListCard/GenreList"
 import Populer from "@/components/ListCard/Populer"
 import Viewed from "@/components/ListCard/Viewed"
+import mY from "@/assets/20250128_230226.png"
 
 const KomikList = () => {
     return (
         <div>
             <div className="flex flex-col items-center justify-center p-2">
                 <div className="flex">
-                    <img src="../assets/20250128_230226.png" alt="Eclipse" className="w-40 h-auto" />
+                    <img src={mY} alt="Eclipse" className="w-40 h-auto" />
                 </div>
                 <span className="text-xs -mt-0">Baca Komik Bahasa Indonesia</span>
             </div>
